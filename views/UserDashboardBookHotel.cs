4abfcc08d9173999f555aa40c5af34fc13b8b6e2
papelopi01 <papel.opi.01@gmail.com>
2025-09-13 22:20:38 +0600

    user all forms

---
+++ views/UserDashboardBookHotel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimus_Travels.views
+{
+    public partial class UserDashboardBookHotel : Form
+    {
+        public UserDashboardBookHotel()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            UserDashboard ud = new UserDashboard();
+            ud.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserDashboardBookHotelBook udhb = new UserDashboardBookHotelBook();
+            udhb.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ views/UserDashboardShowProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimus_Travels.views
+{
+    public partial class UserDashboardShowProfile : Form
+    {
+        public UserDashboardShowProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserDashboard ud = new UserDashboard();
+            ud.Show();
+            this.Hide();
+        }
+    }
+}
